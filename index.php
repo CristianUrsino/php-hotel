bba40ad4297c452cd2a21facad810010336f9219
CristianUrsino <ursino.cristian.uc@gmail.com>
2023-11-29 15:41:00 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include __DIR__ . '/partials/header.php'; ?>
+<body>
+    <main><?php var_dump($hotels) ?></main>
+    <?php include __DIR__ . '/partials/footer.php'; ?>
+</body>
+</html>
---
+++ partials/header.php
@@ -0,0 +1,14 @@
+<?php
+include __DIR__ . '/../Model/db.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP-HOTEL</title>
+</head>
+<body>
+    <header>HEADER</header>
+</body>
